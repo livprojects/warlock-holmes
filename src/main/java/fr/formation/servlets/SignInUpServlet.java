0dc52ac0adbb7d58923a5144a16cd31de580af88
Sarayachak <sarahchloe.chakra@gmail.com>
2021-09-10 14:11:30 +0200

    quickfix on signup servlet and jsp

--- src/main/java/fr/formation/servlets/SignInUpServlet.java
+++ src/main/java/fr/formation/servlets/SignInUpServlet.java
@@ -2,14 +2,20 @@ package fr.formation.servlets;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class SignInUpServlet
  */
+@WebServlet(
+		name="signupServlet",
+		urlPatterns= {"/signup"},
+		loadOnStartup = 1
+		)
 public class SignInUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -25,8 +31,8 @@ public class SignInUpServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		this.getServletContext().getRequestDispatcher("/WEB-INF/views/signup.jsp").forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
