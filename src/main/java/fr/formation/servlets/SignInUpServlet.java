5f17d1b9b7f3c5953c68aa1cd89524afe114af2d
Sarayachak <sarahchloe.chakra@gmail.com>
2021-09-10 12:05:30 +0200

    added signup servlet + jsp working

--- src/main/java/fr/formation/servlets/SignInUpServlet.java
+++ src/main/java/fr/formation/servlets/SignInUpServlet.java
@@ -26,6 +26,7 @@ public class SignInUpServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/views/signup.jsp").forward(request, response);
 	}
 
 	/**
