c61f585138bfa1e39e51546e007e23fd2cf6c3d6
livprojects <liv.audigane@gmail.com>
2021-10-07 16:29:45 +0200

    config router

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- TODO : déplacer dans une nav -->
+      <!-- <router-link to="/">Accueil</router-link> |
+      <router-link to="/complaints">Bureau des plaintes</router-link> -->
     </div>
     <router-view/>
   </div>
 </template>
+
 
 <style>
 #app {
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +11,28 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/complaints',
+    name: 'Complaints',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/Complaints.vue')
+  }, 
+  {
+    path: '/clues',
+    name: 'Clues',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/Clues.vue')
+  },
+  {
+    path: '/detective',
+    name: 'DetectivesOffice',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/Detective.vue')
   }
 ]
 
