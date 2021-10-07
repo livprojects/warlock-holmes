251063a8161802057cbd5d3df84fc6aebb70d1e8
Sarayachak <sarahchloe.chakra@gmail.com>
2021-10-07 14:56:37 +0200

    script relational database, also on pgadmin

--- conception/script_BDD/bdd.sql
+++ conception/script_BDD/bdd.sql
@@ -2,43 +2,59 @@
 -- CREATE DATABASE db_name COLLATE utf8_general_ci;
 -- USE db_name;
 
-CREATE TABLE Utilisateur(
-	nom varchar(255),
-	prenom varchar(255),
-	pseudo varchar(255),
-	email varchar(255) NOT NULL,
-	password varchar(255)
+CREATE TABLE Player(
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name varchar(50),
+	surname varchar(50),
+	pseudo varchar(50),
+	email varchar(100) NOT NULL,
+	password varchar(25)
 );
 
-INSERT INTO Utilisateur VALUES ('Saraya', 'Chak', 'Saraya', 'sarayachak.com', 'secret');
-INSERT INTO Utilisateur VALUES ('Liv', 'Audigane', 'Lili', 'livaudi.com', 'admin');
-INSERT INTO Utilisateur VALUES ('Jonathan', 'Triqueneau', 'JojoTrinqueno', 'jojo.com', 'admin');
+INSERT INTO Player VALUES ('Saraya', 'Chak', 'Saraya', 'sarayachak.com', 'secret');
+INSERT INTO Player VALUES ('Liv', 'Audigane', 'Lili', 'livaudi.com', 'admin');
+INSERT INTO Player VALUES ('Jonathan', 'Triqueneau', 'JojoTrinqueno', 'jojo.com', 'admin');
 
 
 
-CREATE TABLE Plainte(
-	plaignant varchar(255),
-	procesVerbal varchar(255),
-	urlIllustration varchar(255)
+CREATE TABLE Complaints(
+	id INTEGER NOT NULL UNIQUE GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	plaintiveName varchar(50),
+	report varchar(255),
+	urlIllustration varchar(100)
+	
 );
 
-INSERT INTO Plainte(plaignant, procesVerbal, urlIllustration) VALUES ('John Duff', 'Lorem ipsum blablablou', 'blabla');
+INSERT INTO Complaints(plaintiveName, report, urlIllustration) VALUES ('John Duff', 'Lorem ipsum blablablou', 'blabla');
 
 
 CREATE TABLE Suspect(
-	nom varchar(255),
-	prenom varchar(255),
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name varchar(255),
+	surname varchar(255),
 	occupation varchar(255),
 	alibi varchar(255),
 	urlPhoto varchar(255)
 );
 
-INSERT INTO Suspect(nom, prenom, occupation, alibi, urlPhoto) VALUES ('Sarah', 'Chakra', 'Directrice de M2i', 'Jétais pas au cinéma', 'blabla');
+INSERT INTO Suspect(name, surname, occupation, alibi, urlPhoto) VALUES ('Sarah', 'Chakra', 'Directrice de M2i', 'Jétais pas au cinéma', 'blabla');
 
 
-CREATE TABLE Indice(
+CREATE TABLE Clue(
+	id INTEGER NOT NULL PRIMARY KEY,
+	FOREIGN KEY (id) REFERENCES Complaints(id),
 	description varchar(255),
 	imageUrl varchar(255)
+	
 );
 
-INSERT INTO Indice(description, imageUrl) VALUES ('Un couteau très coupant', 'blabla')
+INSERT INTO Clue(id, description, imageUrl) VALUES (1, 'Un couteau très coupant', 'blabla')
+
+CREATE TABLE ClueSuspectRelation(
+	ClueId Int NOT NULL UNIQUE,
+	SuspectId Int NOT NULL UNIQUE,
+	FOREIGN KEY (ClueId) REFERENCES Clue(id),
+	FOREIGN KEY (SuspectId) REFERENCES Suspect(id)
+);
+
+--------------------------------------------------------------------------------
