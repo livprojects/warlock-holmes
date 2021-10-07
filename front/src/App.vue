fcf001b60d5418937199b03ad734ef2eb493ab90
livprojects <liv.audigane@gmail.com>
2021-10-07 15:57:25 +0200

    modifs vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Complaints></Complaints>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Complaints from './components/Complaints.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    Complaints
   }
 }
 </script>
--- front/src/components/Complaints.vue
+++ front/src/components/Complaints.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>{{ msg }}</h1>
+ <div>
+     <header>
+             <h1>Warlock Holmes</h1>  
+             <h2>Le bureau des plaintes</h2>
+         </header>
+     
+         <main>
+             <div class="clues-left-nav">
+                 <!-- Inclure la navigation administrative -->
+             </div>
+     
+     
+             <div class="clues-main-closet">
+             <p>Tu es ici au point de départ de l'enquête. Sur ce tableau, tu trouveras toutes les affaires que nous avons enregistrées et qui n'ont pas encore été résolues à ce jour.</p>               
+                 <div v-for="complaint in listeTest" :key="complaint.plaignant" class="plainte">
+                     <p>Nom du ou de la plaignante : {{complaint.plaignant}}</p>
+                     <p>Procès verbal : {{complaint.procesVerbal}} </p>		
+                      <img :src="complaint.urlIllustration"/>  
+                 </div>
+             </div>
+     
+     
+             <div class="clues-right-nav">
+                <!-- Inclure la navigation narrative -->
+             </div>
+         </main>
+ </div>
 </template>
 
 <script>
@@ -7,16 +33,30 @@ export default {
   name: 'Complaints',
   data() {
     return {
-      msg: ''
+    listeTest:[
+        {
+        plaignant: "Toto", 
+        procesVerbal:"Tatata", 
+        urlIllustration: "https://static.posters.cz/image/1300/posters-muraux/cute-kitten-i77094.jpg"
+        }, 
+                {
+        plaignant: "Ttutu", 
+        procesVerbal:"patatati patatata", 
+        urlIllustration: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJMvPpFn8ZPh19QDb2JnwuXvgmmEUyMlJoKg&usqp=CAU"
+        }
+        ],
+    //   complaintsList: []
     }
   },
-  mounted() {
-    fetch("/api/complaints/")
-      .then((response) => response.text())
-      .then((data) => {
-          this.msg = data;
-      });
-  }
+//   mounted() {
+//     fetch("/api/complaints/")
+//       .then((response) => response.text())
+//       .then((data) => {
+//           this.complaintsList = data;
+//       });
+//   },
+    methods: {
+    }
 }
 </script>
 
