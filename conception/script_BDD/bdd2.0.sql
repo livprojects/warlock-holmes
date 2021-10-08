592363f9166de0ea05d2aac1fb79e1e97f2cbb98
livprojects <liv.audigane@gmail.com>
2021-10-08 17:35:16 +0200

    renommage bdd et models currentcase

--- conception/script_BDD/bdd2.0.sql
+++ conception/script_BDD/bdd2.0.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE Player(
+CREATE TABLE player(
     player_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     player_name varchar(50),
     player_surname varchar(50),
@@ -7,7 +7,7 @@ CREATE TABLE Player(
     player_email varchar(100) NOT NULL,
     player_pw varchar(25)
 );
-CREATE TABLE Suspect(
+CREATE TABLE suspect(
     suspect_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     suspect_name varchar(255),
     suspect_nickname varchar(255),
@@ -15,18 +15,18 @@ CREATE TABLE Suspect(
     suspect_alibi varchar(255),
     suspect_url_photo varchar(255)
 );
-CREATE TABLE Clue(
+CREATE TABLE clue(
     clue_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     clue_description varchar(255),
     clue_image_url varchar(255)
 );
-CREATE TABLE Affaire(
-    affaire_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    affaire_plaintive_name varchar(50),
-    affaire_report varchar(255),
-    affaire_url_illustration varchar(100),   
-    affaire_suspect_id int,
-    affaire_clue_id int,
-    FOREIGN KEY (affaire_suspect_id) references suspect(suspect_id),
-    FOREIGN KEY (affaire_clue_id) references clue(clue_id)
+CREATE TABLE current_case(
+    current_case_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    current_case_plaintive_name varchar(50),
+    current_case_report varchar(255),
+    current_case_url_illustration varchar(100),   
+    current_case_suspect_id int,
+    current_case_clue_id int,
+    FOREIGN KEY (current_case_suspect_id) references suspect(suspect_id),
+    FOREIGN KEY (current_case_clue_id) references clue(clue_id)
 );
