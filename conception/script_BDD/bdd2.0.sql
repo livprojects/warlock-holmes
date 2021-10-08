ad735821970c7543963aa9f03d298a0bc209a2d5
Jaune9 <prinnydad@hotmail.fr>
2021-10-08 17:55:51 +0200

    INSERT INTO

--- conception/script_BDD/bdd2.0.sql
+++ conception/script_BDD/bdd2.0.sql
@@ -7,6 +7,13 @@ CREATE TABLE player(
     player_email varchar(100) NOT NULL,
     player_pw varchar(25)
 );
+
+
+INSERT INTO Player(player_name, player_surname, player_pseudo, player_email, player_pw) VALUES ('Saraya', 'Chak', 'Saraya', 'sarayachak.com', 'secret');
+INSERT INTO Player(player_name, player_surname, player_pseudo, player_email, player_pw) VALUES ('Liv', 'Audigane', 'Lili', 'livaudi.com', 'admin');
+INSERT INTO Player(player_name, player_surname, player_pseudo, player_email, player_pw) VALUES ('Jonathan', 'Triqueneau', 'JojoTrinqueno', 'jojo.com', 'Marie');
+
+
 CREATE TABLE suspect(
     suspect_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     suspect_name varchar(255),
@@ -15,11 +22,19 @@ CREATE TABLE suspect(
     suspect_alibi varchar(255),
     suspect_url_photo varchar(255)
 );
+
+INSERT INTO Suspect(suspect_name, suspect_nickname, suspect_occupation, suspect_alibi, suspect_url_photo) VALUES ('Sarah', 'Chakra', 'Directrice de M2i', 'Jétais pas au cinéma', 'blabla');
+INSERT INTO Suspect(suspect_name, suspect_nickname, suspect_occupation, suspect_alibi, suspect_url_photo) VALUES ('Cépamoi', 'Célui', 'Mangeuse de bonbon', 'La main bloquée dans la jare à cookies', 'nope');
+
 CREATE TABLE clue(
     clue_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     clue_description varchar(255),
     clue_image_url varchar(255)
 );
+
+INSERT INTO Clue(clue_id, clue_description, clue_image_url) VALUES ('Un couteau très coupant', 'blabla');
+INSERT INTO Clue(clue_id, clue_description, clue_image_url) VALUES ('Une mauvaise foi en acier trempé', 'bliblou');
+
 CREATE TABLE current_case(
     current_case_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     current_case_plaintive_name varchar(50),
@@ -31,3 +46,4 @@ CREATE TABLE current_case(
     FOREIGN KEY (current_case_clue_id) references clue(clue_id)
 );
 
+INSERT INTO current_case(current_case_plaintive_name, current_case_report, current_case_url_illustration, current_case_suspect_id, current_case_clue_id) VALUES ('John Duff', 'Lorem ipsum blablablou', 'blabla', 0, 0);
