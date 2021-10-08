3984e33f1248200c11e269be9ab8c44013318c3e
livprojects <liv.audigane@gmail.com>
2021-10-08 12:36:08 +0200

    ajout validation form bureau detective

--- front/src/components/Detective.vue
+++ front/src/components/Detective.vue
@@ -2,19 +2,24 @@
     <div id="detective">
         <h2>Le bureau du détective</h2>
         <div>
-            <p>Alors, tu penses avoir trouvé notre coupable ? Moi je pense que c'est le colonel Moutarde, avec le chandelier dans... pardon, je m'égare. Alors, dis-moi tout, je coffre qui ?!</p>
+            <p>Alors, tu penses avoir trouvé notre coupable ? Moi je pense que c'est le colonel Moutarde, avec le
+                chandelier dans... pardon, je m'égare. Alors, dis-moi tout, je coffre qui ?!</p>
         </div>
 
 
         <div class="validation-form">
-            <!-- Insérer composant validation formulaire -->
+            <ValidationForm></ValidationForm>
         </div>
 
-        <img src="https://www.greenberg-art.com/.Illustrations/.Humorous/qq1sgMessyDesk.jpg" alt="Bureau de détective"/>
+        <img src="https://www.greenberg-art.com/.Illustrations/.Humorous/qq1sgMessyDesk.jpg"
+            alt="Bureau de détective" />
     </div>
 </template>
 
 <script>
+
+import ValidationForm from './ValidationForm.vue';
+
     export default {
         name: 'Detective',
         //   mounted() {
@@ -24,7 +29,10 @@
         //           this.complaintsList = data;
         //       });
         //   },
-        methods: {}
+        methods: {},
+        components: {
+            ValidationForm
+        }
     }
 </script>
 
---
+++ front/src/components/ValidationForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="validation-form">
+        <form method="POST">
+            <label for="complaint-select">L'affaire/la plainte:</label>
+
+            <select name="complaints" id="complaint-select">
+                <option value="">-- Vas-y, choisis une affaire !--</option>
+                <!-- Mettre une boucle  -->
+            </select>
+
+
+            <label for="suspect-select">Le coupable:</label>
+
+            <select name="suspects" id="suspect-select">
+                <option value="">-- Alors, c'est qui ?--</option>
+                <!-- Mettre une boucle  -->
+            </select>
+
+            <label for="suspect-select">L'arme du crime:</label>
+
+            <select name="suspects" id="suspect-select">
+                <option value="">-- Qu'est-ce que c'est que cette chose ?--</option>
+                <!-- Mettre une boucle  -->
+            </select>
+
+        </form>
+        <button type="submit">Chef(fe), j'ai raison ?</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ValidationForm'
+    }
+</script>
+
+<style>
+    @import '../assets/css/validationform.css';
+</style>
