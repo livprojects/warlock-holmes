23ba846e717e6f73cbb5003d579984fa1ac3d686
Sarayachak <sarahchloe.chakra@gmail.com>
2021-10-08 11:06:12 +0200

    added basic navbar template with css

---
+++ front/src/components/NavBarLieux.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+         <main>
+             <nav class="navbar-places-right">
+                 <!-- Début de la navigation des lieux -->
+             
+
+             <div class="heading">
+                 <h4> Navigation <h4>
+                
+            </div>
+
+            <ul class='nav-links'>
+
+                <li class='nav-link'> <router-link to="/">Accueil</router-link> </li>
+                <li class='nav-link'> <router-link to="/detective">Bureau du détective</router-link> </li>
+                <li class='nav-link'> <router-link to="/complaints">Bureau des plaintes</router-link> </li>
+                <li class='nav-link'> <router-link to="/clues"> Indices </router-link> </li>
+                <li class='nav-link'> <router-link to="/suspects"> Suspects </router-link> </li>
+                <li class='nav-link'> <router-link to="/cases"> Affaires </router-link> </li>
+
+            </ul>
+     
+     
+         </nav>
+         </main>
+ </div>
+</template>
+
+<style>
+@import '../assets/css/navbar.css';
+</style>
