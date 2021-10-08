2459541243bb6a056f89c4a804014f7fc065af70
livprojects <liv.audigane@gmail.com>
2021-10-08 16:13:51 +0200

    fix models

--- src/main/java/fr/formation/models/Suspect.java
+++ src/main/java/fr/formation/models/Suspect.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 @Entity
+@Table(name = "suspect")
 public class Suspect {
 
 	@Id
